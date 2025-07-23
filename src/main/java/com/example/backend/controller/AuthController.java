2af637aedee75012dc60c503e38ec493a4cf363b
Dhanashri Mahade <dhanashrimahade@gmail.com>
2025-07-23 08:59:50 +0530

    Initial commit for this branch

---
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        if ("dhanashri".equals(user.getUsername()) && "dm1234".equals(user.getPassword())) {
+            return "Welcome Dhanashri!";
+        } else if ("admin".equals(user.getUsername()) && "admin1234".equals(user.getPassword())) {
+            return "Welcome Admin!";
+        } else {
+            return "Invalid credentials";
+        }
+    }
+
+    static class User {
+        private String username;
+        private String password;
+
+        // Getters and Setters
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
